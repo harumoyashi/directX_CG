967f21262a8e2ce331d658c67d7545bc49acb397
晴彦 相川 <k021g1126@g.neec.ac.jp>
2022-04-27 17:51:15 +0900

    画像表示完了

--- main.cpp
+++ main.cpp
@@ -302,25 +302,45 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	rootParams[1].DescriptorTable.NumDescriptorRanges = 1;						//デスクリプタレンジ数
 	rootParams[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;				//全てのシェーダーから見える
 
-	//横方向ピクセル数
-	const size_t textureWidth = 256;
-	//縦方向ピクセル数
-	const size_t textureHeight = 256;
-	//配列の要素数
-	const size_t imageDataCount = textureWidth * textureHeight;
-	//画像イメージデータ配列
-	XMFLOAT4* imageData = new XMFLOAT4[imageDataCount];	//※必ず後で解放する
+	////横方向ピクセル数
+	//const size_t textureWidth = 256;
+	////縦方向ピクセル数
+	//const size_t textureHeight = 256;
+	////配列の要素数
+	//const size_t imageDataCount = textureWidth * textureHeight;
+	////画像イメージデータ配列
+	//XMFLOAT4* imageData = new XMFLOAT4[imageDataCount];	//※必ず後で解放する
 
-	//全ピクセルの色を初期化
-	for (size_t i = 0; i < imageDataCount; i++)
+	////全ピクセルの色を初期化
+	//for (size_t i = 0; i < imageDataCount; i++)
+	//{
+	//	imageData[i].x = 1.0f;	//R
+	//	imageData[i].y = 0.0f;	//G
+	//	imageData[i].z = 0.0f;	//B
+	//	imageData[i].w = 1.0f;	//A
+	//}
+
+	TexMetadata metadate{};
+	ScratchImage scratchImg{};
+	//WICテクスチャのロード
+	result = LoadFromWICFile(
+		L"Resources/itiro_kimegao.png",
+		WIC_FLAGS_NONE,
+		&metadate, scratchImg);
+
+	ScratchImage mipChain{};
+	//ミップマップ生成
+	result = GenerateMipMaps(
+		scratchImg.GetImages(), scratchImg.GetImageCount(), scratchImg.GetMetadata(),
+		TEX_FILTER_DEFAULT, 0, mipChain);
+	if (SUCCEEDED(result))
 	{
-		imageData[i].x = 1.0f;	//R
-		imageData[i].y = 0.0f;	//G
-		imageData[i].z = 0.0f;	//B
-		imageData[i].w = 1.0f;	//A
+		scratchImg = std::move(mipChain);
+		metadate = scratchImg.GetMetadata();
 	}
 
-	
+	//読み込んだディフューズテクスチャをSRGBとして扱う
+	metadate.format = MakeSRGB(metadate.format);
 
 	//頂点データ構造体
 	struct Vertex
@@ -458,11 +478,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//リソース設定
 	D3D12_RESOURCE_DESC textureResouceDesc{};
 	textureResouceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
-	textureResouceDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-	textureResouceDesc.Width = textureWidth;	//幅
-	textureResouceDesc.Height = textureHeight;	//高さ
-	textureResouceDesc.DepthOrArraySize = 1;
-	textureResouceDesc.MipLevels = 1;
+	textureResouceDesc.Format = metadate.format;
+	textureResouceDesc.Width = metadate.width;	//幅
+	textureResouceDesc.Height = (UINT)metadate.height;	//高さ
+	textureResouceDesc.DepthOrArraySize = (UINT16)metadate.arraySize;
+	textureResouceDesc.MipLevels = (UINT16)metadate.mipLevels;
 	textureResouceDesc.SampleDesc.Count = 1;
 
 	//テクスチャバッファの生成
@@ -477,25 +497,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		IID_PPV_ARGS(&texBuff)
 	);
 
-	//テクスチャバッファにデータ転送
-	result = texBuff->WriteToSubresource(
-		0,
-		nullptr,	//全領域へコピー
-		imageData,	//元データアドレス
-		sizeof(XMFLOAT4) * textureWidth,	//1ラインサイズ
-		sizeof(XMFLOAT4) * imageDataCount	//全サイズ
-	);
-
-	//元データ解放
-	delete[] imageData;
+	//全ミップマップについて
+	for (size_t i = 0; i < metadate.mipLevels; i++)
+	{
+		//ミップマップレベルを指定してイメージを取得
+		const Image* img = scratchImg.GetImage(i, 0, 0);
+		//テクスチャバッファにデータ転送
+		result = texBuff->WriteToSubresource(
+			(UINT)i,
+			nullptr,				//全領域へコピー
+			img->pixels,			//元データアドレス
+			(UINT)img->rowPitch,	//1ラインサイズ
+			(UINT)img->slicePitch	//全サイズ
+		);
+		assert(SUCCEEDED(result));
+	}
 
 	//シェーダーリソースビューの作成
 	//シェーダーリソースビュー設定
 	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};	//設定構造体
-	srvDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;	//RGBA float
+	srvDesc.Format = resDesc.Format;	//RGBA float
 	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
 	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;	//2Dテクスチャ
-	srvDesc.Texture2D.MipLevels = 1;
+	srvDesc.Texture2D.MipLevels = resDesc.MipLevels;
 
 	//ハンドルの指す位置にシェーダーリソースビュー作成
 	device->CreateShaderResourceView(texBuff, &srvDesc, srvHandle);
