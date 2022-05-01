7cf9be285efafcd846d07b3103773509e5a2f47b
晴彦 相川 <k021g1126@g.neec.ac.jp>
2022-05-02 04:40:49 +0900

    理解深めただけ

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -1,14 +1,14 @@
 #include "Basic.hlsli"
 
-//float4 main() : SV_TARGET
-//{
-//	return color;
-//}
+float4 main() : SV_TARGET
+{
+	return color;
+}
 
 Texture2D<float4>tex:register(t0);	//0番スロットに設定されたテクスチャ
 SamplerState smp : register(s0);	//0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	return float4(tex.Sample(smp,input.uv));
+	return float4(tex.Sample(smp,input.uv)) /** color*/;	//定数バッファに送った色変えると反映される
 }
