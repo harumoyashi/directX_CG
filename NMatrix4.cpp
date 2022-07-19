ed4edc7ddc93a1405cbbb4c1f4135a74097cad86
晴彦 相川 <k021g1126@g.neec.ac.jp>
2022-07-19 23:41:39 +0900

    クラス置き場のほうも更新

--- NMatrix4.cpp
+++ NMatrix4.cpp
@@ -1,45 +1,127 @@
 #include "NMatrix4.h"
+#include <cmath>
 
-//Matrix4& Matrix4::operator*=(const Matrix4& m2)
-//{
-//	Matrix4 result;
-//	for (size_t i = 0; i < 4; i++)
-//	{
-//		for (size_t j = 0; j < 4; j++)
-//		{
-//			for (size_t k = 0; k < 4; k++)
-//			{
-//				result.m[i][j] += m[i][k] * m2.m[k][j];
-//			}
-//		}
-//	}
-//	return result;
-//}
-
-Matrix4& Matrix4::operator*=(const Vector3 scalar)
+Matrix4 identity()
 {
-	m[0][0] *= scalar.x;
-	m[1][0] *= scalar.x;
-	m[2][0] *= scalar.x;
-	m[3][0] *= scalar.x;
-
-	m[0][1] *= scalar.y;
-	m[1][1] *= scalar.y;
-	m[2][1] *= scalar.y;
-	m[3][1] *= scalar.y;
-
-	m[0][2] *= scalar.z;
-	m[1][2] *= scalar.z;
-	m[2][2] *= scalar.z;
-	m[3][2] *= scalar.z;
-	return *this;
+    static const Matrix4 result
+    {
+        1.0f,0.0f,0.0f,0.0f,
+        0.0f,1.0f,0.0f,0.0f,
+        0.0f,0.0f,1.0f,0.0f,
+        0.0f,0.0f,0.0f,1.0f
+    };
+    return result;
 }
 
-Matrix4 IdentityMatrix(Matrix4 matrix)
+Matrix4 scale(const Vector3& s)
 {
-	for (int i = 0; i < 4; i++)
-	{
-		matrix.m[i][i] = 1;
-	}
-	return matrix;
+    Matrix4 result
+    {
+        s.x,0,0,0,
+        0,s.y,0,0,
+        0,0,s.z,0,
+        0,0,0,1
+    };
+    return result;
+}
+
+Matrix4 rotateX(float angle)
+{
+    float sin = std::sin(angle);
+    float cos = std::cos(angle);
+
+    Matrix4 result
+    {
+        1.0f,0.0f,0.0f,0.0f,
+        0.0f,cos,sin,0.0f,
+        0.0f,-sin,cos,0.0f,
+        0.0f,0.0f,0.0f,1.0f
+    };
+    return result;
+}
+
+Matrix4 rotateY(float angle)
+{
+    float sin = std::sin(angle);
+    float cos = std::cos(angle);
+
+    Matrix4 result
+    {
+        cos,0.0f,-sin,0.0f,
+        0.0f,1.0f,0.0f,0.0f,
+        sin,0.0f,cos,0.0f,
+        0.0f,0.0f,0.0f,1.0f
+    };
+    return result;
+}
+
+Matrix4 rotateZ(float angle)
+{
+    float sin = std::sin(angle);
+    float cos = std::cos(angle);
+
+    Matrix4 result
+    {
+        cos,sin,0.0f,0.0f,
+        -sin,cos,0.0f,0.0f,
+        0.0f,0.0f,1.0f,0.0f,
+        0.0f,0.0f,0.0f,1.0f
+    };
+    return result;
+}
+
+Matrix4 translate(const Vector3& t)
+{
+    Matrix4 result
+    {
+        1.0f,0.0f,0.0f,0.0f,
+        0.0f,1.0f,0.0f,0.0f,
+        0.0f,0.0f,1.0f,0.0f,
+        t.x,t.y,t.z,1.0f
+    };
+    return result;
+}
+
+Vector3 transform(const Vector3& v, const Matrix4& m)
+{
+    float w = v.x * m.m[0][3] + v.y * m.m[1][3] + v.z * m.m[2][3] + m.m[3][3];
+
+    Vector3 result
+    {
+        (v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + m.m[3][0]) / w,
+        (v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + m.m[3][1]) / w,
+        (v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + m.m[3][2]) / w
+    };
+
+    return result;
+}
+
+Matrix4& operator*=(Matrix4& m1, const Matrix4& m2)
+{
+    Matrix4 result{ 0 };
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+            }
+        }
+    }
+    m1 = result;
+    return m1;
+}
+
+const Matrix4 operator*(const Matrix4& m1, const Matrix4& m2)
+{
+    Matrix4 result = m1;
+
+    return result *= m2;
+}
+
+const Vector3 operator*(const Vector3& v, const Matrix4& m)
+{
+    return transform(v, m);
 }
